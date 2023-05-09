21930287a9814d66b2f99ab1a5e52e4d260ed41d
wobblyrunner <parsr.mail@gmail.com>
2023-05-09 13:51:13 -0700

    Add vuetify to client app

--- Journals.Client/src/main.ts
+++ Journals.Client/src/main.ts
@@ -3,9 +3,11 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import vuetify from '@/vuetify'
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
