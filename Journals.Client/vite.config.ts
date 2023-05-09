6169552a70f3738625ee3ba7ec86b93105b3d3c1
wobblyrunner <parsr.mail@gmail.com>
2023-05-09 11:19:11 -0700

    Initial Commit

---
+++ Journals.Client/vite.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vuetify(),
+    basicSsl(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    https: true,
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:7286/',
+        changeOrigin: false,
+        secure: false,
+      }
+    }
+  }
+})
